32fed2d1c1eb443702b244448f4591828d54d460
szymon_nowak <szymon.nowak@solarboat.agh.edu.pl>
2024-10-18 00:29:13 +0200

    algo na 1 laby

--- opt_alg.cpp
+++ opt_alg.cpp
@@ -32,13 +32,47 @@ solution MC(matrix(*ff)(matrix, matrix, matrix), int N, matrix lb, matrix ub, do
 
 double* expansion(matrix(*ff)(matrix, matrix, matrix), double x0, double d, double alpha, int Nmax, matrix ud1, matrix ud2)
 {
-	try
-	{
-		double* p = new double[2]{ 0,0 };
-		//Tu wpisz kod funkcji
+	try {
+        double* p = new double[2]{0, 0};
+        int i = 0;
+        double x1 = x0 + d;
 
-		return p;
-	}
+		double x[2] = { x0, x1 };
+        
+        if (ff(x[1], ud1, ud2) == ff(x[0], ud1, ud2)) {
+            p[0] = x[0];
+            p[1] = x[1];
+            return p;
+        }
+
+		if (ff(x[1], ud1, ud2) > ff(x[0], ud1, ud2)) {
+			d = -d;
+			x[1] = x[0] + d;
+			if (ff(x[1], ud1, ud2) >= ff(x[0], ud1, ud2)) {
+				p[0] = x[0];
+				p[1] = x[0] - d;
+				return p;
+			}
+		}
+        while (ff(x[1], ud1, ud2) <= ff(x[0], ud1, ud2)) {
+        
+            if (solution::f_calls > Nmax) {
+                throw std::runtime_error("error");
+            }
+
+            i++;
+            x[1] = x0 + pow(alpha, i) * d;
+        }  
+
+        if (d > 0) {
+            p[0] = x[0];
+            p[1] = x[1];
+        } else {
+            p[0] = x[1];
+            p[1] = x[0];
+        }
+        return p;
+    }
 	catch (string ex_info)
 	{
 		throw ("double* expansion(...):\n" + ex_info);
@@ -50,8 +84,32 @@ solution fib(matrix(*ff)(matrix, matrix, matrix), double a, double b, double eps
 	try
 	{
 		solution Xopt;
-		//Tu wpisz kod funkcji
 
+		int k = 1;
+    	std::vector<double> fi = {0, 1};
+
+		while (fib[k] < (b - a) / epsilon) {
+			k++;
+			fi.push_back(fi[k - 1] + fi[k - 2]);
+    	}
+
+		double a[2] = {a, 0};
+		double b[2] = {b, 0};
+		double c[2] = {(b[0] - fi[k-1]) / (fi[k] * (b[0] - a[0])), 0};
+		double d[2] = {a[0] + b[0] - c[0], 0};
+
+		for (int i = 0; i < k - 3; i++) {
+			if (ff(c[0], ud1, ud2) < ff(d[0], ud1, ud2)) {
+				a[1] = a[0];
+				b[1] = d[0];
+			} else {
+				b[1] = b[0];
+				a[1] = c[0];
+			}
+			c[1] = b[1] - fi[k-i-2] / (fi[k-i-1] * (b[1] - a[1]));
+			d[1] = a[1] + b[1] - c[1];
+    	}
+		Xopt.x = c[1]
 		return Xopt;
 	}
 	catch (string ex_info)
@@ -61,13 +119,60 @@ solution fib(matrix(*ff)(matrix, matrix, matrix), double a, double b, double eps
 
 }
 
-solution lag(matrix(*ff)(matrix, matrix, matrix), double a, double b, double epsilon, double gamma, int Nmax, matrix ud1, matrix ud2)
+solution lag(matrix(*ff)(matrix, matrix, matrix), double a0, double b0, double epsilon, double gamma, int Nmax, matrix ud1, matrix ud2)
 {
 	try
 	{
 		solution Xopt;
-		//Tu wpisz kod funkcji
+		int i = 0;
+		double c0 = (b0 + a0) / 2;
+        double a[2] = {a0, 0}; 
+        double b[2] = {b0, 0}; 
+        double c[2] = {c0, 0}; 
+        double d[2] = {0, 0};
+		do {
+			double l = ff(a[0], ud1, ud2) * (b[0] * b[0] - c[0] * c[0]) +
+					   ff(b[0], ud1, ud2) * (c[0] * c[0] - a[0] * a[0]) + 
+					   ff(c[0], ud1, ud2) * (a[0] * a[0] - b[0] * b[0]);
+			double m = ff(a[0], ud1, ud2) * (b[0] - c[0]) + 
+					   ff(b[0], ud1, ud2) * (c[0] - a[0]) + 
+					   ff(c[0], ud1, ud2) * (a[0] - b[0]);
+			if (m <= 0)
+				throw string("error1");
+			d[0] = d[1];
+			d[1] = 0.5 * l / m;
+			if(a[0] < d[1] && d[1] < c[0]) {
+				if (ff(d[1], ud1, ud2) < ff(c[0], ud1, ud2)) {
+					a[1] = a[0];
+					c[1] = d[1];
+					b[1] = c[0];
+				} else {
+					a[1] = d[1];
+					c[1] = c[0];
+					b[1] = b[1];
+				}
+			} else {
+				if (c[0] < d[1] && d[1] < b[0]) {
+					if (ff(d[1], ud1, ud2) < ff(c[0], ud1, ud2)) {
+						a[1] = c[0];
+						c[1] = d[1];
+						b[1] = b[0];
+					} else {
+						a[1] = a[0];
+						c[1] = c[0];
+						b[1] = d[1];
+					}
+				} else {
+					throw string("error2");
+				}
+				i++;
+				if (solution::f_calls > Nmax) {
+                	throw std::runtime_error("error3");
+            	}
+			}
+		} while ( (b[0] - a[0]) < epsilon || abs(d[1] - d[0]) < gamma );
 
+		Xopt.x = d[1];
 		return Xopt;
 	}
 	catch (string ex_info)
